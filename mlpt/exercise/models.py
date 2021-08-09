81ac960108da86a97696170bcd0a1bc6e1e0f00a
purgat0ry <casey@camccarthy.com>
2021-08-09 21:02:20 +0900

    ammo weights

--- mlpt/exercise/models.py
+++ mlpt/exercise/models.py
@@ -58,22 +58,62 @@ class EquipmentItemManager(models.Manager):
                 )
             )
 
+
 class AmmoItemManager(models.Manager):
 
-    def get_ammo_weight(self, exercise: int) -> dict:
-        """Return the cumulative weight of all ammo types associated with all equipment across an exercise."""
+    def get_total_base_allowance_weight(self, exercise: int) -> dict:
+        """Return the cumulative base allowance weight of all ammo types associated with all equipment across an exercise."""
         return self.get_queryset().filter(
             equipment__in=Equipment.objects.filter(
-                exercise__in=Exercise.objects.filter(id=exercise)
+                equipmentitem__in=EquipmentItem.objects.filter(
+                    exercise__in=Exercise.objects.filter(id=exercise)
+                    )
                 )
-            ).aggregate(ammo_weight=Sum(F('ammo__weight') * F('quantity')))
+            ).aggregate(total_ammo_base_allowance=Sum(
+                F('ammo__weight') * 
+                F('ammo__ammoitem__base_allocation') *
+                F('equipment__equipmentitem__quantity')
+                )
+            )
 
 
-class EquipmentCombatLoadManager(models.Manager):
+    def get_total_daily_assault_weight(self, exercise: int) -> dict:
+        """Return the cumulative daily assault replenishment weight of all ammo types associated with all equipment across an exercise."""
+        return self.get_queryset().filter(
+            equipment__in=Equipment.objects.filter(
+                equipmentitem__in=EquipmentItem.objects.filter(
+                    exercise__in=Exercise.objects.filter(id=exercise)
+                    )
+                )
+            ).aggregate(total_ammo_daily_assault=Sum(
+                F('ammo__weight') * 
+                F('ammo__ammoitem__daily_assault') *
+                F('equipment__equipmentitem__quantity')
+                )
+            )
+
+    def get_total_daily_sustain_weight(self, exercise: int) -> dict:
+        """Return the cumulative daily sustain replenishment weight of all ammo types associated with all equipment across an exercise."""
+        return self.get_queryset().filter(
+            equipment__in=Equipment.objects.filter(
+                equipmentitem__in=EquipmentItem.objects.filter(
+                    exercise__in=Exercise.objects.filter(id=exercise)
+                    )
+                )
+            ).aggregate(total_ammo_daily_sustain=Sum(
+                F('ammo__weight') * 
+                F('ammo__ammoitem__daily_sustain') *
+                F('equipment__equipmentitem__quantity')
+                )
+            )
+
+
+class CombatLoadManager(models.Manager):
 
     def combat_loads(self, equipment: int) -> dict:
         """Return a list of all Combat Loads for a particular weapon system."""
         pass
+
 
 # ammo
 class Ammo(models.Model):
@@ -84,36 +124,9 @@ class Ammo(models.Model):
     def __str__(self):
         return self.name
 
+
 class AmmoItem(models.Model):
     """A reference to Ammo that will exist in an exercise edl."""
-
-    ammo = models.ForeignKey(Ammo, on_delete=models.CASCADE)
-
-    ASSAULT = 'AE'
-    FOLLOW_ON = 'AFOE'
-
-    PHASING_LOCATION = [
-        (ASSAULT, 'Assault Element'),
-        (FOLLOW_ON, 'Assault Follow-On Element'),
-
-    ]
-
-    phasing_location = models.CharField(
-        max_length=4,
-        choices=PHASING_LOCATION,
-        default=ASSAULT,
-        help_text="The location of the item in relation to the phasing."
-    )
-
-    quantity = models.PositiveIntegerField(default=0, help_text="The quantity of an item.")
-
-    objects = AmmoItemManager()
-
-    def __str__(self):
-        return self.ammo.name
-
-class EquipmentCombatLoadItem(models.Model):
-    """A reference to teh combat load for a peice of equipment."""
 
     ammo = models.ForeignKey(Ammo, on_delete=models.CASCADE)
 
@@ -136,13 +149,16 @@ class EquipmentCombatLoadItem(models.Model):
     daily_assault = models.PositiveIntegerField(default=0, help_text="The daily sustainment quantity.")
     daily_sustain = models.PositiveIntegerField(default=0, help_text="The daily sustainment quantity.")
 
-    objects = EquipmentCombatLoadManager()
+    objects = AmmoItemManager()
 
-    def __str__(self):
+    @property
+    def name(self):
         return self.ammo.name
 
+    def __str__(self):
+        return self.name
 
-
+        
 # equipment
 class Equipment(models.Model):
     """A peice of equipment may belong to multiple units and multiple exercises at once."""
@@ -152,12 +168,15 @@ class Equipment(models.Model):
     burn_rate = models.FloatField(default=0, help_text="The burn rate of fuel in gal/h.")
     weight = models.FloatField(default=0, help_text="The weight of the equipment in pounds.")
     combat_loads = models.ManyToManyField(
-        EquipmentCombatLoadItem, 
+        AmmoItem,
+        through='CombatLoad',
+        through_fields=('equipment','ammoitem',),
         help_text="A list of equipment ammo that this equipment contains."
         )
 
     def __str__(self):
         return self.name
+
 
 class EquipmentItem(models.Model):
     """A reference to equipment that will exist in an exercise edl."""
@@ -189,6 +208,19 @@ class EquipmentItem(models.Model):
 
 
 
+class CombatLoad(models.Model):
+    """A reference to teh combat load for a peice of equipment."""
+
+    
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    ammoitem = models.ForeignKey(AmmoItem, on_delete=models.CASCADE)
+
+
+
+    objects = CombatLoadManager()
+
+    def __str__(self):
+        return self.ammoitem.name
 
 # unit
 class Unit(models.Model):
@@ -197,21 +229,13 @@ class Unit(models.Model):
 
     def __str__(self):
         return self.name
-
-# troop - officer, enlisted, etc
-class PeopleType(models.Model):
-    """PeopleType can belong to many units."""
-    name = models.CharField(max_length=50, help_text="The title/role of a type of person.")
-
-    def __str__(self):
-        return self.name      
+    
 
 # exercise
 class Exercise(models.Model):
     """A exercise will be the primary object, composed of other objects that are associated with the exercise."""
     name = models.CharField(max_length=128)
-    equipments = models.ManyToManyField(EquipmentItem, through="ExerciseEquipmentItem", through_fields=('exercise', 'equipment'),)
-    people_types = models.ManyToManyField(PeopleType, through="ExercisePeopleType", through_fields=('exercise', 'people_type'),)
+    equipments = models.ManyToManyField(EquipmentItem, through="ExerciseEdl", through_fields=('exercise', 'equipment'),)
     units = models.ManyToManyField(Unit)
     objects = ExerciseManager()
 
@@ -222,7 +246,7 @@ class Exercise(models.Model):
         return self.name
 
 
-class ExerciseEquipmentItem(models.Model):
+class ExerciseEdl(models.Model):
     """Intermediate table for Exercises and Equipments assigning a quantity."""
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
@@ -231,16 +255,5 @@ class ExerciseEquipmentItem(models.Model):
     def __str__(self):
         return f"{self.exercise}  {self.unit}  {self.equipment} "
 
-
-class ExercisePeopleType(models.Model):
-    """Intermediate table for Exercises and PeopleTypess assigning a quantity."""
-    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    people_type = models.ForeignKey(PeopleType, on_delete=models.CASCADE)
-
-
-
-    def __str__(self):
-        return f"{self.exercise}  {self.unit}  {self.people_type}  "
 
 
--- mlpt/exercise/views.py
+++ mlpt/exercise/views.py
@@ -11,13 +11,14 @@ def home(request):
     #quantities = Exercise.objects.get_equipment_quantity(exercise=exercise.id)
     #fuel = Exercise.objects.get_equipment_bingo_fuel(exercise=exercise.id)
     #equipment_ammos = Exercise.objects.get_equipment_ammo_count(exercise=exercise.id)
-    ammo_weight = AmmoItem.objects.get_ammo_weight(exercise=exercise.id)
+    base_allowance = AmmoItem.objects.get_total_base_allowance_weight(exercise=exercise.id)
+    daily_assault = AmmoItem.objects.get_total_daily_assault_weight(exercise=exercise.id)
+    daily_sustain = AmmoItem.objects.get_total_daily_sustain_weight(exercise=exercise.id)
   
 
 
     # get all equipments going and list their ammos
     #ammos_list = ExerciseEquipment.objects.filter(exercise=exercise.id).values('equipment__name').annotate(count=Sum(F('equipment__ammos__weight') * F('quantity')))
-    print(ammo_weight)
     #ammos_total = ExerciseEquipment.objects.filter(exercise=exercise.id).aggregate(total_weight=Sum(F('equipment__weight') * F('quantity')))
 
     
@@ -32,7 +33,9 @@ def home(request):
 
     context = {
         
-        'ammo_weight' : ammo_weight,
+        'base_allowance' : base_allowance,
+        'daily_assault' : daily_assault,
+        'daily_sustain' : daily_sustain,
 
       
     }
