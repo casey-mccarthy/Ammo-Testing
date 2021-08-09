72da3b446e5098cf0b01a586346c4c9bddb5b766
purgat0ry <casey@camccarthy.com>
2021-08-09 23:33:49 +0900

    progress

--- mlpt/exercise/admin.py
+++ mlpt/exercise/admin.py
@@ -4,9 +4,7 @@ from . import models
 # Register your models here.
 admin.site.register(models.Exercise)
 admin.site.register(models.Equipment)
-admin.site.register(models.EquipmentItem)
 admin.site.register(models.Ammo)
-admin.site.register(models.AmmoItem)
 admin.site.register(models.Unit)
 admin.site.register(models.ExerciseEdl)
 admin.site.register(models.CombatLoad)
--- mlpt/exercise/views.py
+++ mlpt/exercise/views.py
@@ -6,36 +6,27 @@ from .models import *
 
 
 def home(request):
+    """Testing view to demonstrate new structure and methods."""
     exercise = Exercise.objects.all().first()
     #weight = Exercise.objects.get_equipment_weight(exercise=exercise.id)
     #quantities = Exercise.objects.get_equipment_quantity(exercise=exercise.id)
     #fuel = Exercise.objects.get_equipment_bingo_fuel(exercise=exercise.id)
     #equipment_ammos = Exercise.objects.get_equipment_ammo_count(exercise=exercise.id)
-    base_allowance = AmmoItem.objects.get_total_base_allowance_weight(exercise=exercise.id)
-    daily_assault = AmmoItem.objects.get_total_daily_assault_weight(exercise=exercise.id)
-    daily_sustain = AmmoItem.objects.get_total_daily_sustain_weight(exercise=exercise.id)
+    base_allowance = Ammo.objects.get_total_base_allowance_weight(exercise=exercise.id)
+    #daily_assault = Ammo.objects.get_total_daily_assault_weight(exercise=exercise.id)
+    #daily_sustain = Ammo.objects.get_total_daily_sustain_weight(exercise=exercise.id)
   
-
 
     # get all equipments going and list their ammos
     #ammos_list = ExerciseEquipment.objects.filter(exercise=exercise.id).values('equipment__name').annotate(count=Sum(F('equipment__ammos__weight') * F('quantity')))
     #ammos_total = ExerciseEquipment.objects.filter(exercise=exercise.id).aggregate(total_weight=Sum(F('equipment__weight') * F('quantity')))
 
-    
-
-
-
-
-    
-
-    
-
-
+ 
     context = {
         
         'base_allowance' : base_allowance,
-        'daily_assault' : daily_assault,
-        'daily_sustain' : daily_sustain,
+        #'daily_assault' : daily_assault,
+        #'daily_sustain' : daily_sustain,
 
       
     }
